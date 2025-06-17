889e42b8203e8f30ba6d9466fd1205f23ab6364f
Sambeg Shrestha <sambeg.shrestha@konnectcraft.com>
2025-06-17 21:02:53 +0545

    fix: fix newDate

--- src/newDate/test.ts
+++ src/newDate/test.ts
@@ -3,13 +3,13 @@ import { newDate } from "./index.js";
 
 describe("newDate", function () {
   it("create a date", function () {
-    const result = newDate(1393, 6 /* Mehr */, 8);
-    expect(result).toEqual(/* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30));
+    const result = newDate(2071, 5 /* Mehr */, 14);
+    expect(result).toEqual(/* 2071/6/14 */ new Date(2014, 8 /* Sep */, 30));
   });
   it("create a date with time", function () {
-    const result = newDate(1393, 6 /* Mehr */, 8, 20, 40);
+    const result = newDate(2071, 5 /* Mehr */, 14, 20, 40);
     expect(result).toEqual(
-      /* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30, 20, 40),
+      /* 2071/6/14 */ new Date(2014, 8 /* Sep */, 30, 20, 40),
     );
   });
 });
